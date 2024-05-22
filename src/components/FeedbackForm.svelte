3c96c4f02b3bd89bd2f4800b2c8e717952b60782
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2024-05-22 19:54:52 +0300

    Add: Submitting new feedback

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import { v4 as uuid } from 'uuid';
+  import Card from './Card.svelte';
+  import Button from './Button.svelte';
+  import RatingSelect from './RatingSelect.svelte';
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+
+  let text = '';
+  let rating = 10;
+  let min = 10;
+  let message = '';
+  let btnDisabled = true;
+
+  const handleInput = (e: Event) => {
+    const target = e.target as HTMLInputElement;
+    text = target.value;
+    if (text.trim().length < min) {
+      message = `Text must be at least ${min} characters`;
+      btnDisabled = true;
+    } else {
+      message = '';
+      btnDisabled = false;
+    }
+  };
+
+  const handleSelect = (e: CustomEvent<number>) => {
+    rating = e.detail;
+  };
+
+  const handleSubmit = () => {
+    if (text.trim().length >= min) {
+      const newFeedback = { id: uuid(), text, rating };
+      dispatch('add-feedback', newFeedback);
+      text = '';
+    }
+  };
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+  <form on:submit|preventDefault={handleSubmit}>
+    <RatingSelect on:rating-select={handleSelect} />
+    <div class="input-group">
+      <input
+        type="text"
+        bind:value={text}
+        placeholder="Tell us something that keeps you coming back"
+        on:input={handleInput}
+      >
+      <Button disabled={btnDisabled} type="submit">Send</Button>
+    </div>
+    {#if message}
+      <div class="message">{message}</div>
+    {/if}
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  .message {
+    padding-top: 10px;
+    text-align: center;
+    color: rebeccapurple;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
   import FeedbackList from '../components/FeedbackList.svelte';
+  import FeedbackStats from '../components/FeedbackStats.svelte';
+  import FeedbackForm from '../components/FeedbackForm.svelte';
+  import type { Feedback } from '../models/types';
 
   let feedback = [
     { id: 1, text: 'This is a test', rating: 5 },
@@ -7,12 +10,21 @@
     { id: 3, text: 'This is another test', rating: 4 },
   ];
 
+  $:count = feedback.length;
+  $:average = feedback.reduce((acc, item) => acc + item.rating, 0) / count;
+
   const deleteFeedback = (e: CustomEvent<number>) => {
     const itemId = e.detail;
     feedback = feedback.filter((item) => item.id !== itemId);
   };
+
+  const addFeedback = (e: CustomEvent<Feedback>) => {
+    feedback = [e.detail, ...feedback];
+  };
 </script>
 
 <main class="container">
+  <FeedbackForm on:add-feedback={addFeedback} />
+  <FeedbackStats {count} {average} />
   <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
